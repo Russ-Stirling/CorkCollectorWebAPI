36d05f846b8ae367f4418905510e7bd199e79e85
russ-stirling <rstirli2@uwo.ca>
2018-01-17 10:23:03 -0500

    Added UI for data batch operation utility

---
+++ CorkCollector.Utility/DisplayMenu.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorkCollector.Utility
+{
+    class DisplayMenu
+    {
+        public DisplayMenu()
+        {
+            
+        }
+
+        public bool run(string rootDir)
+        {
+            bool done = false;
+            string subMenu = GetMainMenu();
+            string operation = string.Empty;
+            switch (subMenu)
+            {
+                case "1":
+                    operation = GetBatchDataMenu();
+                    break;
+            }
+            switch (string.Format("{0}-{1}", subMenu, operation))
+            {
+                case "1-1":
+                    break;
+                case "2-1":
+                    break;
+                case "2-2":
+                    //FileOrganizer.
+                    break;
+                default:
+                    done = CheckIfDone();
+                    break;
+            }
+            return done;
+        }
+
+        private bool CheckIfDone()
+        {
+            string[] options = new string[2] { "1", "2" };
+            bool correctInput = false;
+            string choice = null;
+
+            while (!correctInput)
+            {
+                Console.WriteLine("1. Return to Main Menu");
+                Console.WriteLine("2. Exit Application");
+
+                choice = Console.ReadLine();
+
+                if (options.Contains(choice))
+                    correctInput = true;
+                else
+                    Console.WriteLine("Invalid input. Please select one of the menu options.");
+            }
+
+            return choice == "1" ? true : false;
+
+        }
+
+        private string GetMainMenu()
+        {
+            string[] options = new string[1] { "1" };
+            bool correctInput = false;
+            string choice = null;
+
+            while (!correctInput)
+            {
+                Console.WriteLine("1. Batch Data Operations");
+
+                choice = Console.ReadLine();
+
+                if (options.Contains(choice))
+                    correctInput = true;
+                else
+                    Console.WriteLine("Invalid input. Please select one of the menu options.");
+            }
+
+            return choice;
+        }
+
+        private string GetBatchDataMenu()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ CorkCollector.Utility/Options.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace CorkCollector.Utility
+{
+    class Options
+    {
+        [Option('h', "headless", Default = false,
+            HelpText = "Run application with parameters, without the menu")]
+        public bool Headless { get; set; }
+
+        //[Option('r', "read", Required = true,
+        //HelpText = "Input file to be processed.")]
+        //public string InputFile { get; set; }
+
+        //[Option('v', "verbose", DefaultValue = true,
+        //  HelpText = "Prints all messages to standard output.")]
+        //public bool Verbose { get; set; }
+
+    }
+}
