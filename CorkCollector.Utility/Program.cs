e370fdb3dc140c55bbb3e00f21b16bc7788ad769
russ-stirling <rstirli2@uwo.ca>
2018-01-17 14:57:17 -0500

    Successful communication with https authenticated database

--- CorkCollector.Utility/Program.cs
+++ CorkCollector.Utility/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
-using Raven.Client.Document;
+using CorkCollector.Data;
+using Raven.Client.Documents;
 
 namespace CorkCollector.Utility
 {
@@ -11,12 +13,24 @@ namespace CorkCollector.Utility
     {
         static void Main(string[] args)
         {
+            var Cert = new X509Certificate2();
+            Cert.Import("D:\\CorkCollector\\DBServer\\CorkCollectorTest.pfx", "Cork123", X509KeyStorageFlags.DefaultKeySet);
             var store = new DocumentStore
             {
-                ConnectionStringName = "RavenDB/CorkCollector"
+                Database = "CorkCollector",
+                Urls = new string[] {"https://a.corkcollector.dbs.local.ravendb.net:8080"},
+                Certificate = Cert
+
             };
 
-            
+            store.Initialize();
+
+            var x = store.OpenSession();
+
+            x.Store(new Wine());
+
+            x.SaveChanges();
+
         }
     }
 }
