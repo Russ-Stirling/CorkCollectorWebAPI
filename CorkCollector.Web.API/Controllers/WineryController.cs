efbc2595af9ebcacffcc5311a8a32aece6cde348
russ-stirling <rstirli2@uwo.ca>
2018-01-18 13:23:29 -0500

    Global raven session created for accessing ravendb. Winery get routes implemented

---
+++ CorkCollector.Web.API/Controllers/WineryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using CorkCollector.Data;
+using Newtonsoft.Json;
+
+namespace CorkCollector.Web.API.Controllers
+{
+    public class WineryController : ApiController
+    {
+        // GET api/winery       route: api/winery       returns: All wineries
+        public string Get()
+        {
+            List<Winery> wineries = new List<Winery>();
+            using (var session = WebApiApplication.RavenStore.OpenSession())
+            {
+                wineries = session.Query<Winery>().ToList();
+            }
+
+            return JsonConvert.SerializeObject(wineries);
+        }
+
+        // GET api/winery/id         route: api/winery?id=wineries/[id]     Returns: SPecified winery
+        public string Get(string id)
+        {
+            Winery winery = new Winery();
+            using (var session = WebApiApplication.RavenStore.OpenSession())
+            {
+                winery = session.Load<Winery>(id);
+            }
+
+            return JsonConvert.SerializeObject(winery);
+        }
+    }
+}
--- CorkCollector.Web.API/Global.asax.cs
+++ CorkCollector.Web.API/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Raven.Client.Documents;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -11,6 +13,9 @@ namespace CorkCollector.Web.API
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private X509Certificate2 Cert;
+        public static DocumentStore RavenStore; 
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -18,6 +23,22 @@ namespace CorkCollector.Web.API
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Cert = new X509Certificate2();
+
+            //TODO: move pfx location and password to app config
+            Cert.Import("D:\\CorkCollector\\DBServer\\CorkCollectorTest.pfx", "Cork123", X509KeyStorageFlags.DefaultKeySet);
+
+            RavenStore = new DocumentStore
+            {
+                Database = "CorkCollector",
+                Urls = new string[] { "https://a.corkcollector.dbs.local.ravendb.net:8080" },
+                Certificate = Cert
+            };
+
+            RavenStore.Initialize();
+
+
         }
     }
 }
