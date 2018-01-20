c6d7c37c80a19ae3cee5c3f657d58c9f6424c708
russ-stirling <rstirli2@uwo.ca>
2018-01-20 15:19:00 -0500

    Test case fucking finished. Why the hell the security protocol was trying to do TLS 1.1 i don't know but its 1.2 proper now

---
+++ CorkCollector.Test/TestCaseBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using CorkCollector.Web.API.Controllers;
+using Raven.Client.Documents;
+using Xunit;
+
+namespace CorkCollector.Test
+{
+    public class TestCaseBase
+    {
+        private readonly X509Certificate2 Cert;
+        private readonly DocumentStore RavenStore;
+        private readonly WineryController wineryController;
+
+        public TestCaseBase()
+        {
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            Cert = new X509Certificate2();
+
+            //TODO: move pfx location and password to app config
+            Cert.Import("D:\\CorkCollector\\DBServer\\CorkCollectorTest.pfx", "Cork123", X509KeyStorageFlags.DefaultKeySet);
+
+            RavenStore = new DocumentStore
+            {
+                Database = "CorkCollector",
+                Urls = new string[] { "https://a.corkcollector.dbs.local.ravendb.net:8080" },
+                Certificate = Cert
+            };
+
+            RavenStore.Initialize();
+
+            wineryController = new WineryController()
+            {
+                ravenStore = RavenStore
+            };
+        }
+        [Fact]
+        public void PassingTest()
+        {
+            
+            wineryController.Get();
+        }
+    }
+}
--- CorkCollector.Web.API/Controllers/WineryController.cs
+++ CorkCollector.Web.API/Controllers/WineryController.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using CorkCollector.Data;
 using Newtonsoft.Json;
+using Raven.Client.Documents;
 
 namespace CorkCollector.Web.API.Controllers
 {
     public class WineryController : ApiController
     {
+        public DocumentStore ravenStore;
+
+        public WineryController()
+        {
+            if(ravenStore==null)
+                { ravenStore = WebApiApplication.RavenStore;}
+        }
+
         // GET api/winery       route: api/winery       returns: All wineries
         public string Get()
         {
             List<Winery> wineries = new List<Winery>();
-            using (var session = WebApiApplication.RavenStore.OpenSession())
+            using (var session = ravenStore.OpenSession())
             {
                 wineries = session.Query<Winery>().ToList();
             }
@@ -27,7 +37,7 @@ namespace CorkCollector.Web.API.Controllers
         public string Get(string id)
         {
             Winery winery = new Winery();
-            using (var session = WebApiApplication.RavenStore.OpenSession())
+            using (var session = ravenStore.OpenSession())
             {
                 winery = session.Load<Winery>(id);
             }
